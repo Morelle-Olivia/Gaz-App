5747b5c6470bfe2a3ed679c93692bac9b05e4a93
Morelle-Olivia <hotoumorelle@gmail.com>
2022-03-09 08:15:41 +0100

    Work on the signUp

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
+  signupForm!: FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.signupForm = this.formBuilder.group(
+      {
+        name: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.compose([Validators.required, Validators.minLength(8), Validators.maxLength(256)])],
+        role: ['', Validators.required]
+      }
+    );
+  }
+  get f(): {[key: string]: AbstractControl} {
+    return this.signupForm.controls;
+  }
+
+  onSubmit(): void{
+    this.submitted = true;
+    if (this.signupForm.invalid){
+      return;
+    }
   }
 
 }
